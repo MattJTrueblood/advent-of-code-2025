3e7ff5f442eb31bcbcbddd9002317bb4ff37401e
Matthew Trueblood <mtrueblood@d-a-s.com>
2026-09-21 16:16:32 -0400

    working on nontrivial solution

--- 12/part1.py
+++ 12/part1.py
@@ -9,6 +9,7 @@ def parseLines(lines):
             "shape": current_present_lines[1:4],
         }
         current_present["size"] = getPresentSize(current_present)
+        current_present = findAllRotations(current_present)
         presents.append(current_present)
 
     for line in lines[30:]:
@@ -21,6 +22,19 @@ def parseLines(lines):
     print(presents)
     print(regions)
     return presents, regions
+
+def findAllRotations(present):
+    grid = tuple(present['shape'])
+    unique_shapes = set()
+    for _ in range(4):
+        grid = tuple("".join(row) for row in zip(*grid[::-1])) # rotate
+        flipped = tuple(row[::-1] for row in grid) # flip
+        unique_shapes.add(grid)
+        unique_shapes.add(flipped)
+    return {
+        'shapes': [list(shape) for shape in unique_shapes], 
+        'size': present['size']
+    }
 
 def getPresentSize(present):
     size = 0
@@ -38,9 +52,15 @@ def getSolution(presents, regions):
         if sumPresentsSize > totalRegionSize:
             continue # impossible region, too many presents no matter how you arrange them
         else:
-            # this is good enough for input.txt but I'd like to try a proper solver some time
-            numGoodRegions += 1
+            if presentsCanAllFit(presents, region):
+                numGoodRegions += 1
     return numGoodRegions
+
+def presentsCanAllFit(presents, region):
+    # TODO
+    print(presents)
+    print(region)
+    return True
 
 solution = 0
 with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "input.txt")) as f:
